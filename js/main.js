9647bf1207009c2b5ed2bf0e4f1a768d8b756ff5
Vadim Hermann <vhermann@wheregroup.com>
2013-04-17 10:09:51 +0200

    add some page design; add the editor container with switch functionality;

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(function(){
+  $("#home, #editor").bind("click", function(){
+    var me = $(this);
 
+    $("#contentWrapper").removeClass("home")
+                        .removeClass("editor")
+                        .addClass(me.attr("id"));
+    $("#nav").find(".active").removeClass("active");
+    me.addClass("active");
+
+    return false;
+  });
+});
