7873d0eb12d92e58ed159df4b1deaeadc05c281a
Vaddo <contact@vadim-hermann.com>
2013-06-26 23:35:23 +0200

    new page markup... the old one was boring XD

--- js/main.js
+++ js/main.js
@@ -13,39 +13,8 @@ $(function(){
     languanizr.reloadLanguage("http://localhost/languanizr.js/js/german.json");
     return false;
   });
-  // languanizr.setOptions({languagePack:"http://192.168.0.103/languanizr/js/english.json", permanent:true}).loadLanguage();
-
-  // home, editor navigation
-  $("#home, #editor").bind("click", function(){
-    var me = $(this);
-
-    $("#contentWrapper").removeClass("home")
-                        .removeClass("editor")
-                        .removeClass("json")
-                        .addClass(me.attr("id"));
-    $("#nav").find(".active").removeClass("active");
-    me.addClass("active");
-
-    return false;
-  });
 
   $("#tour").bind("click", function(){
-    introJs().onbeforechange(function(targetElement) {
-      var cur = $(".introjs-helperNumberLayer").text();
-
-      if(cur == 4){
-        $("#editor").click();
-      }
-      if(cur == 8){
-        var e = jQuery.Event("keydown");
-        e.ctrlKey = true;
-        e.keyCode = 69
-        $("thead .langCol:first").focus().trigger(e);
-      }
-    }).start();
-  });
-
-  $("#backEditor").bind("click", function(){
-    $("#editor").click();
+    introJs().start();
   });
 });
